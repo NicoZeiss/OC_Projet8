73255eb988a3f496a7a6d9238231e03b814cd701
nicolas <zeissern@gmail.com>
2019-12-03 03:27:17 +0100

    add first views and urls

--- PurBeurre/comparator/models.py
+++ PurBeurre/comparator/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+# from django.db import models
 
 # Create your models here.
+
+ALIMENTS = {
+	'saucisson': {'name': 'Le bon saucisson'},
+	'jambon': {'name': 'Le jambon qui tue'},
+	'roquefort': {'name': 'Le formage trop fort'},
+	'biquette': {'name': 'Le chèvre de biquette'},
+}
+
+CATEGORIES = [
+	{'name': 'Viandes', 'aliments': [ALIMENTS['saucisson'], ALIMENTS['jambon']]},
+	{'name': 'Fromages', 'aliments': [ALIMENTS['roquefort'], ALIMENTS['biquette']]},
+]
--- PurBeurre/comparator/views.py
+++ PurBeurre/comparator/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import CATEGORIES
+
+def index(request):
+	message = "Salut tout le monde !"
+	return HttpResponse(message)
+
+def listing(request):
+	message = "La liste de produits ici :"
+	return HttpResponse(message)
+
+def search(request):
+	query = request.GET.get('query')
+
+	if not query:
+		message = "Aucun aliment n'est demandé"
+	else:
+		categories = [
+			categorie for categorie in CATEGORIES
+			if query in " ".join(aliment['name'] for aliment in categorie['aliments'])
+		]
+
+		if len(categories) == 0:
+			message = "Misère, pas de résultat trouvé"
+		else:
+			categories = ["<li>{}</li>".format(categorie['name']) for categorie in categories]
+			message = """
+				Nous avons trouvé les catégories correspondant à votre requête, les voici :
+				<ul>
+					{}
+				</ul>
+				""".format("</li><li>".join(categories))
+
+	return HttpResponse(message)
