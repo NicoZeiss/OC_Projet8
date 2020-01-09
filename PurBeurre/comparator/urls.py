feec100f2046ff7f07b1a95cd30a6ed647b8c1d8
nicolas <zeissern@gmail.com>
2020-01-09 23:39:40 +0100

    add substitute results

--- PurBeurre/comparator/urls.py
+++ PurBeurre/comparator/urls.py
@@ -6,4 +6,5 @@ app_name = 'comparator'
 urlpatterns = [
 	url(r'^search/$', views.search, name="search"),
 	url(r'^(?P<bar_code>[0-9]+)/$', views.detail, name="detail"),
+	url(r'^substitute/$', views.substitute, name="substitute"),
 ]
--- PurBeurre/comparator/views.py
+++ PurBeurre/comparator/views.py
@@ -43,3 +43,20 @@ def detail(request, bar_code):
 
 	return render(request, 'comparator/detail.html', context)
 
+def substitute(request):
+	query = request.GET.get('user-substitute')
+	user_choice = Food.objects.get(code=query)
+	choice_cat = user_choice.category_id
+	full_list = Food.objects.filter(category_id=choice_cat).order_by('nutriscore')
+	sub_list = []
+	i = 0
+	while i <= 8:
+		sub_list.append(full_list[i])
+		i += 1
+
+	context = {
+		"sublist": sub_list,
+		"query": user_choice,
+	}
+
+	return render(request, 'comparator/substitute.html', context)
