0f78e33541919764e335c8a9ceb5afac1498d88d
nicolas <zeissern@gmail.com>
2020-01-10 20:04:41 +0100

    finish add all main func

--- PurBeurre/comparator/urls.py
+++ PurBeurre/comparator/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
 	url(r'^search/$', views.search, name="search"),
 	url(r'^(?P<bar_code>[0-9]+)/$', views.detail, name="detail"),
 	url(r'^substitute/$', views.substitute, name="substitute"),
+	url(r'^save_sub/$', views.save_sub, name="save_sub"),
+	url(r'^favourites/$', views.favourites, name="favourites"),
 ]
--- PurBeurre/comparator/views.py
+++ PurBeurre/comparator/views.py
@@ -1,9 +1,10 @@
 import django.contrib.postgres
 from django.shortcuts import render, reverse, render_to_response
-from django.http import HttpResponseRedirect
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.contrib.auth.models import User
 from django import forms
+from django.http import JsonResponse
 from .forms import SearchForm
 from .models import Food, Category
 
@@ -34,6 +35,7 @@ def search(request):
 
 	return render(request, 'comparator/search.html', context)
 
+
 def detail(request, bar_code):
 	food = Food.objects.get(code=bar_code)
 
@@ -43,20 +45,58 @@ def detail(request, bar_code):
 
 	return render(request, 'comparator/detail.html', context)
 
+
 def substitute(request):
-	query = request.GET.get('user-substitute')
+	query = request.GET.get('user_substitute')
 	user_choice = Food.objects.get(code=query)
 	choice_cat = user_choice.category_id
 	full_list = Food.objects.filter(category_id=choice_cat).order_by('nutriscore')
-	sub_list = []
+	sub_dic = {}
 	i = 0
-	while i <= 8:
-		sub_list.append(full_list[i])
+	while len(sub_dic) <= 8:
+		food = full_list[i]
+		user = request.user.id
+		if len(food.user.all()) != 0:
+			sub_dic[food] = True
+		else:
+			sub_dic[food] = False
 		i += 1
 
 	context = {
-		"sublist": sub_list,
+		"subdic": sub_dic,
 		"query": user_choice,
 	}
 
 	return render(request, 'comparator/substitute.html', context)
+
+def save_sub(request):
+	query_code = request.GET.get('query_code')
+	sub_code = request.GET.get('sub_code')
+	sub = Food.objects.get(code=sub_code)
+	user_id = request.user.id
+	user = User.objects.get(id=user_id)
+	user.food.add(sub)
+
+	return HttpResponseRedirect('http://127.0.0.1:8000/comparator/substitute/?user_substitute={}'.format(query_code))
+
+
+def favourites(request):
+	user = request.user
+	sub_list = user.food.all().order_by('nutriscore')
+	paginator = Paginator(sub_list, 9)
+	page = request.GET.get("page")
+
+	try:
+		sublist = paginator.page(page)
+	except PageNotAnInteger:
+		sublist = paginator.page(1)
+	except EmptyPage:
+		sublist = paginator.page(paginator.num_pages)
+
+
+	context = {
+		"sublist": sublist
+	}
+
+	return render(request, 'comparator/favourites.html', context)
+
