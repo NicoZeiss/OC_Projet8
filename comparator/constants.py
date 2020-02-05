5bb348a8d7a173d82495fe0e3d175b6915acf596
nicolas <zeissern@gmail.com>
2020-02-05 12:27:18 +0100

    update constants.py

--- comparator/constants.py
+++ comparator/constants.py
@@ -1,4 +1,4 @@
-categories_name = ['boissons-energisantes', 'beignets-sucres', 'taboules', 'pates-a-tartiner', 'popcorn', 'chips', 'biscuits-aperitifs', 'fruits-a-coques-sales', 'eaux-gazeuses', 'laits', 
+categories_name = ['boissons-energisantes', 'beignets-sucres', 'taboules', 'pates-a-tartiner', 'popcorn', 'chips', 'biscuits-aperitifs', 'fruits-a-coque-sales', 'eaux-gazeuses', 'laits', 
 'cremes-fraiches', 'soupes-de-legumes', 'pains-de-mie', 'baguettes', 'cookies', 'biscuits-sables', 'gaufrettes', 'jambons', 'rillettes', 'saucissons', 'boudins', 
 'sauces-tomate', 'moutardes', 'mayonnaises', 'sauces-au-curry', 'sauces-bechamel', 'sauces-pesto', 'sauces-salades', 'fromages-rapes', 'fromages-de-chevre', 'fromages-de-vache', 'compotes', 
 'glaces-a-l-eau',  'sardines', 'thons', 'maquereaux', 'anchois', 'colins', 'saumons', 'ravioli-en-conserve', 'yaourts-aux-fruits', 'yaourts-natures', 'jus-de-fruit', 
